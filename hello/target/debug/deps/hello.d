59c51e30a41a2897f75d63171e4b4a626b823f10
napi <napidev111@gmail.com>
2022-09-11 22:59:30 -0500

    This is a new commit for what I originally planned to be amended

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\Elect\OneDrive\Escritorio\code\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\Elect\OneDrive\Escritorio\code\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\Elect\OneDrive\Escritorio\code\hello\target\debug\hello.exe: C:\Users\Elect\OneDrive\Escritorio\code\hello\src\main.rs
